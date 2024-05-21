078313786372ec85dfc9b2bc75d98802866c34a8
MalkingMan <arrayalkhozini@gmail.com>
2024-05-21 14:48:53 +0700

    commit 1

---
+++ MemberInitializationlist.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa
+{
+private:
+    const int id;
+    string nama;
+    float nilai;
+
+public:
+    mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai)
+    ~mahasiswa();
+};
+
+mahasiswa::mahasiswa(/* args */)
+{
+}
+
+mahasiswa::~mahasiswa()
+{
+}
