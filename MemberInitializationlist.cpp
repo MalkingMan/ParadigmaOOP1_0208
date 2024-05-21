352894200e4a511c0def86920d6df1743eec1c12
MalkingMan <arrayalkhozini@gmail.com>
2024-05-21 14:51:31 +0700

    commit 2

--- MemberInitializationlist.cpp
+++ MemberInitializationlist.cpp
@@ -10,13 +10,20 @@ private:
     float nilai;
 
 public:
-    mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai)
-    ~mahasiswa();
-};
+    mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) { // Member
+        // Definisi
+    }
 
-mahasiswa::mahasiswa(/* args */)
+mahasiswa()
 {
+    cout << "Id     =" << id << endl
+    cout << "Nama   =" << nama << endl
+    cout << "Nilai  =" << Nilai << endl
+
 }
+
+
+
 
 mahasiswa::~mahasiswa()
 {
